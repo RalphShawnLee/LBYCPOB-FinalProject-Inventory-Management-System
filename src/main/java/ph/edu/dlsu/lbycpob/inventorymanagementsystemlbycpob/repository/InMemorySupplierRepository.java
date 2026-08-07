39797a5397526e26bd8e56465f75cbc056e79588
RalphShawnLee <ralph_shawn_lee@dlsu.edu.ph>
2026-08-08 05:12:18 +0800

    add in-memory ProductRepository implementation, defined supplier repository and the findAll/save contract, and added in-memory supplier repo implementation

---
+++ src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/InMemorySupplierRepository.java
@@ -0,0 +1,20 @@
+package ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.repository;
+
+import ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemorySupplierRepository implements SupplierRepository {
+    private final List<Supplier> suppliers = new ArrayList<>();
+
+    @Override
+    public List<Supplier> findAll() {
+        return suppliers;
+    }
+
+    @Override
+    public void save(Supplier supplier) {
+        suppliers.add(supplier);
+    }
+}
--- src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/SupplierRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/SupplierRepository.java
@@ -1,4 +1,11 @@
 package ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.repository;
 
+import ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.model.Supplier;
+
+import java.util.List;
+
 public interface SupplierRepository {
+    List<Supplier> findAll();
+
+    void save(Supplier supplier);
 }
