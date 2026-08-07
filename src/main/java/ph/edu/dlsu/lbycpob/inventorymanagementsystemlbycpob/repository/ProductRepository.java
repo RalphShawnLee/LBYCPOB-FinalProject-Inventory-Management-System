53acdc7a648e839101f7a8329a8d8b278d1d0151
RalphShawnLee <ralph_shawn_lee@dlsu.edu.ph>
2026-08-08 05:10:22 +0800

    define ProductRepository findAll/save contract

--- src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/ProductRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/ProductRepository.java
@@ -1,4 +1,11 @@
 package ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.repository;
 
+import ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.model.Product;
+
+import java.util.List;
+
 public interface ProductRepository {
+    List<Product> findAll();
+
+    void save(Product product);
 }
