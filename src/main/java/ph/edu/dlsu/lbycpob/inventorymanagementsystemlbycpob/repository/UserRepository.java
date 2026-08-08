e216d44bb341b770f8b40064f6be72f5f382fd4b
RalphShawnLee <ralph_shawn_lee@dlsu.edu.ph>
2026-08-08 23:41:36 +0800

    defined UserRepository findByUsername/save contract

--- src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/UserRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.repository;
 
+import ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.model.User;
+
+import java.util.Optional;
+
 public interface UserRepository {
+    Optional<User> findByUsername(String username);
+
+    void save(User user);
 }
