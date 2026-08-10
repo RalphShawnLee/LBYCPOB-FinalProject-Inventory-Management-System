2748ccc80fcad82017214c16a813f68c581f32c3
John Sean Nicole Neri <john_sean_neri@dlsu.edu.ph>
2026-08-10 13:52:43 +0800

    Added NotificationService to check if a product has low stock and needs to be reordered.

--- src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/service/NotificationService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/service/NotificationService.java
@@ -1,4 +1,9 @@
 package ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.service;
 
+import ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.model.Product;
+
 public class NotificationService {
+    public boolean isLowStock(Product product) {
+        return product.getQuantity() <= product.getReorderThreshold();
+    }
 }
