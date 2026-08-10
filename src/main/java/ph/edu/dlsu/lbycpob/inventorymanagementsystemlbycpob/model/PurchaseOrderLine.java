0ffa35a0a7ffd6c4506792be277b446d72a5ed5c
John Sean Nicole Neri <john_sean_neri@dlsu.edu.ph>
2026-08-10 14:11:08 +0800

    Added PurchaseOrderLine.java that stores one product and how many units are being ordered. It is used inside a PurchaseOrder to list all the products being purchased from a supplier.

--- src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/model/PurchaseOrderLine.java
+++ src/main/java/ph/edu/dlsu/lbycpob/inventorymanagementsystemlbycpob/model/PurchaseOrderLine.java
@@ -1,4 +1,24 @@
 package ph.edu.dlsu.lbycpob.inventorymanagementsystemlbycpob.model;
 
 public class PurchaseOrderLine {
+    private final Product product;
+    private final int quantity;
+
+    public PurchaseOrderLine(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return product.getName() + " x " + quantity;
+    }
 }
